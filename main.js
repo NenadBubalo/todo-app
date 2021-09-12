2c3befa06558b3cf98f1a6d53ecd75e9422c0028
nenad <nenad.bubalo@students.ffhs.ch>
2021-09-12 03:15:09 +0200

    Release 12.09.21

---
+++ main.js
@@ -0,0 +1,103 @@
+let icon = document.getElementById("icon");
+let radioButton1 = document.getElementById("radioButton1");
+let radioButton2 = document.getElementById("radioButton2");
+let radioButton3 = document.getElementById("radioButton3");
+let radioButton4 = document.getElementById("radioButton4");
+let radioButton5 = document.getElementById("radioButton5");
+let radioButton6 = document.getElementById("radioButton6");
+let infoContainerActive = document.getElementById("infoContainerActive");
+let infoContainerCompleted = document.getElementById("infoContainerCompleted");
+let infoContainerAll = document.getElementById("infoContainerAll");
+
+let newTodo = document.getElementById("newTodo");
+let addButton = document.getElementById("addButton");
+let output1 = document.getElementById("output1");
+let output2 = document.getElementById("output2");
+let output3 = document.getElementById("output3");
+let output4 = document.getElementById("output4");
+let output5 = document.getElementById("output5");
+let output6 = document.getElementById("output6");
+
+
+icon.onclick = function () {
+    document.body.classList.toggle("light-theme");
+    if(document.body.classList.contains("light-theme")) {
+        icon.src = "../images/icon-moon.svg";
+    }
+    else {
+        icon.src = "../images/icon-sun.svg";
+    }
+}
+
+//ändert die farbe beim klicken auf dem button
+function changeColorButton () {
+    if(this.style.background === "var(--borderColorHover)") {
+        this.style.background = "var(--divColor)";
+    }
+    else {
+        this.style.background = "var(--borderColorHover)";
+    }
+}
+
+//ändert die farbe beim klicken auf den info text
+function changeColorText () {
+    if(this.style.color === "var(--fontActive)") {
+        this.style.color = "var(--fontColor2)";
+    }
+    else {
+        this.style.color = "var(--fontActive)";
+    }
+}
+
+//todos hinzufügen
+addButton.addEventListener("click", function () {
+    var number = 1;
+    var paragraph = document.createElement('p');
+    paragraph.innerText = newTodo.value;
+    if (output1.hasChildNodes() === false) {
+        output1.appendChild(paragraph);
+        newTodo.value = " ";
+    }
+    else if (output2.hasChildNodes() === false) {
+        output2.appendChild(paragraph);
+        newTodo.value = " ";
+    }
+    else if (output3.hasChildNodes() === false) {
+        output3.appendChild(paragraph);
+        newTodo.value = " ";
+    }
+    else if (output4.hasChildNodes() === false) {
+        output4.appendChild(paragraph);
+        newTodo.value = " ";
+    }
+    else if (output5.hasChildNodes() === false) {
+        output5.appendChild(paragraph);
+        newTodo.value = " ";
+    }
+    else if (output6.hasChildNodes() === false) {
+        output6.appendChild(paragraph);
+        newTodo.value = " ";
+    }
+    else {
+        alert("Kein Platz! Bitte löschen Sie ein Todo.");
+    }
+
+})
+
+
+radioButton1.onclick = changeColorButton;
+radioButton2.onclick = changeColorButton;
+radioButton3.onclick = changeColorButton;
+radioButton4.onclick = changeColorButton;
+radioButton5.onclick = changeColorButton;
+radioButton6.onclick = changeColorButton;
+
+infoContainerAll.onclick = changeColorText;
+infoContainerActive.onclick = changeColorText;
+infoContainerCompleted.onclick = changeColorText;
+
+
+
+
+
+
